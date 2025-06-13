8714a5aa2646b284a9d8b3bf6f961b70f103e26d
marques-gen <genelsonmarques@gmail.com>
2025-06-13 14:36:04 +0000

    feat: Alteração estruturas de pastas

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,15 +1,22 @@
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
 
-load_dotenv()
+from sqlalchemy import create_engine
+from app.config import get_env_var
+
+# Caminho absoluto até a raiz do projeto (onde está o .env)
+#env_path = Path(__file__).resolve().parents[1] / ".env"
+#load_dotenv(dotenv_path=env_path)
 
 def get_engine():
-    user = os.getenv("POSTGRES_USER")
-    password = os.getenv("POSTGRES_PASSWORD")
-    host = os.getenv("POSTGRES_HOST")
-    port = os.getenv("POSTGRES_PORT")
-    db = os.getenv("POSTGRES_DB")
+    user = get_env_var("POSTGRES_USER")
+    password = get_env_var("POSTGRES_PASSWORD")
+    host = get_env_var("POSTGRES_HOST")
+    port = get_env_var("POSTGRES_PORT")
+    db = get_env_var("POSTGRES_DB")
+
+    try:
+        int(port)  # valida a porta
+    except ValueError:
+        raise ValueError(f"POSTGRES_PORT ('{port}') não é um número válido.")
 
     url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
     return create_engine(url)
